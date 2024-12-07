31cc0190b09185033deffd6bb686ff9e42d7b29b
CappedMonke <schuergersilasdev@gmail.com>
2024-12-07 16:29:58 +0100

    Fixed bugs, added post id and category

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import argparse
 from playwright.sync_api import sync_playwright
 from credentials import get_credentials
 from supabase import create_client
@@ -6,20 +7,29 @@ import time
 import re
 
 
-# Run this class in a seperate thread
+URL_list = {
+    "personal_feed": "https://tradingedge.club/feed?sort=newest",
+    # A users activity doesn't show ALL posts they have posted, I'm only able to scroll down to a certain point before it stops loading more posts
+    "tearrepresentative56_activity_feed": "https://tradingedge.club/members/29038203/feed"
+}
+
+MIN_POLLING_RATE = 60 * 2 # 2 minutes is the minimum polling rate, this has to be high enough to find a post that is older than the lookback_days
+MAX_LOOKBACK_DAYS = 6 # 7 is already considered a week, posts older than 6 days are obsolete anyway
+MIN_LOOKBACK_DAYS = 1 
+
+
+# This class scrapes the posts from the given url and inserts them into the database
+# Run this class in a seperate thread or process
 # Example:
 # import threading
 # scraper = Scraper()
 # threading.Thread(target=scraper.run).start()
-
-
 class Scraper:
-    def __init__(self, url, polling_rate=60*5, update_rate=3600, lookback_days=3, headless=True, debug=False):
+    def __init__(self, url=URL_list["tearrepresentative56_activity_feed"], polling_rate=60*5, lookback_days=3, headless=True, debug=False):
         self.url = url
-        self.polling_rate = polling_rate
+        self.polling_rate = max(MIN_POLLING_RATE, polling_rate) # 2 minutes is the minimum polling rate
         self.headless = headless
-        self.update_rate = update_rate
-        self.lookback_days = lookback_days
+        self.lookback_days = max(MIN_LOOKBACK_DAYS, min(lookback_days, MAX_LOOKBACK_DAYS)) # 7 is already considered a week
         self.debug = debug
         self.isRunning = True
         self.page = None
@@ -50,9 +60,6 @@ class Scraper:
             self.page.wait_for_url("https://tradingedge.club/spaces/**")
             self.page.goto(self.url, wait_until="networkidle")
             
-            # Scroll down until the last post is older than the lookback_date
-            self.load_all_posts()
-
             # Start the scraping loop
             while self.isRunning:
                 self.scrape_posts()
@@ -74,20 +81,13 @@ class Scraper:
             # Parse the date
             match = re.match(r"Posted (\d+)([dwmy]) ago", last_post_raw_date)
 
-            # If the date is not found (happens when the post is less than 24h old (hour, minute, second, now))
-            if not match:
-                continue
-            
-            value = int(match.group(1))
-            unit = match.group(2)
+            if match:
+                value = int(match.group(1))
+                unit = match.group(2)
 
-            # Stop scrolling down if old enough post is found
-            if unit == "d" and value > self.lookback_days:
-                break
-
-            # This will get triggered if the last post of the currently loaded feed is older than 7 days
-            if unit in ["w", "m", "y"]:
-                break
+                # Stop scrolling down if old enough post is found
+                if unit == "d" and value > self.lookback_days or unit in ["w", "m", "y"]:
+                    return posts
 
             # Scroll down
             self.page.evaluate("window.scrollTo(0, document.body.scrollHeight);")
@@ -97,52 +97,54 @@ class Scraper:
             posts = self.page.query_selector_all("li.feed-item")
             post_count_after_scroll = len(posts)
             if post_count_before_scroll == post_count_after_scroll:
-                break
+                return posts
 
 
     # This function scrapes the url and inserts the posts into the database
-    def scrape_posts(self):        
+    def scrape_posts(self):
         number_new_posts = 0
         number_updated_posts = 0
 
-        posts = self.page.query_selector_all("li.feed-item")
+        posts = self.load_all_posts()
         for post in posts:
-            link = post.query_selector('.feed-item-post').get_attribute('href') if post.query_selector('.feed-item-post') else None
+            id = post.get_attribute('data-post-id')
             author = post.query_selector('.mighty-attribution-name span').inner_text() if post.query_selector('.mighty-attribution-name span') else None
             title = post.query_selector('.feed-item-post-title h1').inner_text() if post.query_selector('.feed-item-post-title h1') else None
             description = post.query_selector('.feed-item-post-description').inner_text() if post.query_selector('.feed-item-post-description') else None
             likes = post.query_selector('.mighty-post-stat-cheer .mighty-post-stat-cheer-count').inner_text() if post.query_selector('.mighty-post-stat-cheer .mighty-post-stat-cheer-count') else "0"
             comments = post.query_selector('.mighty-post-stat-comment .mighty-post-stat-comment-count').inner_text() if post.query_selector('.mighty-post-stat-comment .mighty-post-stat-comment-count') else "0"
+            link = post.query_selector('.feed-item-post').get_attribute('href') if post.query_selector('.feed-item-post') else None
+            category = category = post.query_selector('.post-tag-name').inner_text() if post.query_selector('.post-tag-name') else None
 
             # Check if post exists already
-            post_exists = self.supabase.table("posts").select("link").eq("link", link).execute().data
+            post_exists = self.supabase.table("posts").select("id").eq("id", id).execute().data
             if post_exists:
-                self.update_post(link, author, title, description, likes, comments)
+                self.update_post(id, title, description, likes, comments, link)
                 number_updated_posts += 1
             else:
-                self.add_post(author, title, description, likes, comments, link)
+                self.add_post(id, author, title, description, likes, comments, link, category)
                 number_new_posts += 1
 
-        print(f"Updated {number_new_posts} new posts.")
+        print(f"Updated {number_updated_posts} posts.")
         print(f"Scraped {number_new_posts} new posts.")
 
     
     # We can't just update the post by opening it's link and extracting the data
     # because when opening the link it renders an offcanvas where data like likes and comments are missing
-    def update_post(self, author, title, description, likes, comments, link):
-        # Update the post in the database
+    def update_post(self, id, title, description, likes, comments):
+        # Update the post in the database (only update the fields that can change)
         self.supabase.table("posts").update({
-            "author": author, 
             "title": title, 
             "description": description, 
             "likes": int(likes), 
-            "comments": int(comments)
-        }).eq("link", link).execute()
+            "comments": int(comments),
+        }).eq("id", id).execute()
         
 
-    def add_post(self, author, title, description, likes, comments, link):
+    def add_post(self, id, author, title, description, likes, comments, link, category):
         # Insert the post to the database
         self.supabase.table("posts").insert([{
+            "id": id,
             "author": author, 
             "title": title, 
             "description": description, 
@@ -152,12 +154,14 @@ class Scraper:
             "date": datetime.datetime.now(datetime.timezone.utc).isoformat(),
             "likes": int(likes), 
             "comments": int(comments), 
-            "link": link}
-        ]).execute()
+            "link": link,
+            "category": category
+        }]).execute()
 
 
 class PostData:
-    def __init__(self, author, title, description, date, likes, comments, link):
+    def __init__(self, id, author, title, description, date, likes, comments, link):
+        self.id = id
         self.author = author
         self.title = title
         self.description = description
@@ -167,13 +171,6 @@ class PostData:
         self.link = link
 
 
-URL_list = {
-    "personal_feed": "https://tradingedge.club/feed?sort=newest",
-    # A users activity doesn't show ALL posts they have posted, I'm only able to scroll down to a certain point before it stops loading more posts
-    "tearrepresentative56_activity_feed": "https://tradingedge.club/members/29038203/feed"
-}
-
-
 if __name__ == "__main__":
-    scraper = Scraper(url=URL_list["tearrepresentative56_activity_feed"], debug=True, headless=False)
+    scraper = Scraper(debug=True, headless=False)
     scraper.run()
